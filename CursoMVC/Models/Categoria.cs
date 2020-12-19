2cdba2e9e07d1b6f2cf4337a413bc8c3de380634
Samuel Paes <samuel.paes@propig.com.br>
2020-12-19 17:32:57 -0300

    Criação do primeiro model, context file e a primeira migration Intial Create

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Descricao
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias
+        {
+            get;
+            set;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localhost)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+    }
+}
